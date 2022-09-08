6838cc4271bc82578b63fde2b63f089b575e67a7
Kieran Warwick <kwarwick97@gmail.com>
2022-09-09 00:53:38 +0100

    Test that error and context is carried

--- interceptors/client_test.go
+++ interceptors/client_test.go
@@ -2,35 +2,46 @@ package interceptors
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 )
 
 func TestUnaryClientInterceptor(t *testing.T) {
-	ctx := context.Background()
+	ctx := context.WithValue(context.Background(), beforeInterceptor{}, "beforeInterceptor")
 	grpcInterceptor := UnaryClientInterceptor(interceptor{
 		beforeHandler: func(ctx context.Context) context.Context {
-			return ctx
+			return context.WithValue(ctx, beforeHandlerFunc{}, "beforeHandlerFunc")
 		},
-		afterHandler: func(ctx context.Context, err error) {},
+		afterHandler: func(ctx context.Context, err error) {
+			assert.Equal(t, "beforeInterceptor", ctx.Value(beforeInterceptor{}))
+			assert.Equal(t, "beforeHandlerFunc", ctx.Value(beforeHandlerFunc{}))
+			assert.ErrorContains(t, err, "invoker error")
+		},
 	})
 
 	_ = grpcInterceptor(ctx, t.Name(), nil, nil, nil, func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
-		return nil
+		return errors.New("invoker error")
+
 	})
 }
 
 func TestStreamClientInterceptor(t *testing.T) {
-	ctx := context.Background()
+	ctx := context.WithValue(context.Background(), beforeInterceptor{}, "beforeInterceptor")
 	grpcInterceptor := StreamClientInterceptor(interceptor{
 		beforeHandler: func(ctx context.Context) context.Context {
-			return ctx
+			return context.WithValue(ctx, beforeHandlerFunc{}, "beforeHandlerFunc")
 		},
-		afterHandler: func(ctx context.Context, err error) {},
+		afterHandler: func(ctx context.Context, err error) {
+			assert.Equal(t, "beforeInterceptor", ctx.Value(beforeInterceptor{}))
+			assert.Equal(t, "beforeHandlerFunc", ctx.Value(beforeHandlerFunc{}))
+			assert.ErrorContains(t, err, "invoker error")
+		},
 	})
 
 	_, _ = grpcInterceptor(ctx, nil, nil, t.Name(), func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
-		return nil, nil
+		return nil, errors.New("invoker error")
 	})
 }
